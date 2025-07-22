a57d34cb666c8e35c48f9b68c38f5545452872ae
Moriz Büsing <moriz.buesing@gmail.com>
2025-07-22 22:17:55 +0200

    Display warning if ffmpeg is not installed

--- src/Plugin.php
+++ src/Plugin.php
@@ -5,15 +5,22 @@ use craft\events\DefineGqlTypeFieldsEvent;
 use craft\gql\TypeManager;
 use GraphQL\Type\Definition\Type;
 use craft\events\RegisterTemplateRootsEvent;
+use craft\events\RegisterComponentTypesEvent;
+use craft\services\Fields;
 use craft\web\View;
 use craft\elements\Asset;
 use yii\base\Event;
+use buesing\streamingvideo\fields\StreamingVideoField;
+use Craft;
+use mikehaertl\shellcommand\Command;
 
 class Plugin extends \craft\base\Plugin
 {
     public function init()
     {
         parent::init();
+
+        $this->checkFFmpegAvailability();
 
         // Register the plugin template root for site templates
         \yii\base\Event::on(
@@ -31,6 +38,15 @@ class Plugin extends \craft\base\Plugin
                 $event->behaviors['streamingVideo'] = [
                     'class' => \buesing\streamingvideo\StreamingVideoBehavior::class
                 ];
+            }
+        );
+
+        // Register the StreamingVideoField field type
+        Event::on(
+            Fields::class,
+            Fields::EVENT_REGISTER_FIELD_TYPES,
+            function(RegisterComponentTypesEvent $event) {
+                $event->types[] = StreamingVideoField::class;
             }
         );
 
@@ -52,6 +68,34 @@ class Plugin extends \craft\base\Plugin
                     ];
                 }
             }
+        );
+    }
+
+    private function checkFFmpegAvailability(): void
+    {
+        try {
+            $command = new Command('ffmpeg -version');
+            $command->execute();
+            
+            if ($command->getExitCode() !== 0) {
+                $this->showFFmpegWarning();
+            }
+        } catch (\Exception $e) {
+            $this->showFFmpegWarning();
+        }
+    }
+
+    private function showFFmpegWarning(): void
+    {
+        if (Craft::$app->getRequest()->getIsCpRequest()) {
+            Craft::$app->getSession()->setError(
+                'FFmpeg not found! The Streaming Video plugin requires FFmpeg to be installed on your system.'
+            );
+        }
+
+        Craft::warning(
+            'FFmpeg is not available on this system. The Streaming Video plugin requires FFmpeg for video processing.',
+            'craft-streaming-video'
         );
     }
 }
